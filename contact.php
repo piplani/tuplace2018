921071f67ea3d93ae9bed4d5ab66d36046df6026
Rohan Piplani <piplani.rohan@gmail.com>
2018-10-06 15:28:56 +0530

    contact added
    contact added

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>TU Placement</title>
+	<link rel="stylesheet"  type="" href="">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/semantic-ui@2.2.13/dist/semantic.min.css'>
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+
+<body>
+	<div class="jumbotron jumbotron-main mb-4">
+		<center>
+			<h1>Contact Us</h1><br>
+			<h3><i>We are ready to lead you into the furure of Artifical Intelligence</i></h3>
+		</center>
+	</div>
+
+	<center><h1> Get in touch with us</h1></center><br><br>
+	<div>
+		<center>
+			
+		<!-- <img src = "http://psrana.com/images/rana.jpg"><br> -->
+			<h3><b>Name :</b> Dr.Prashant Singh Rana</h3><br>
+			<h3><b>Email: </b>psrana@gmail.com</h3><br>
+			<h3><b>Phone:</b> +91-9855764471; +91-9313889932</h3><br>
+			<h3><b>Position:</b> Assistant Professor at Computer Science and Engineering Department</h3><br>
+		</center>
+	</div>	
+</body>
